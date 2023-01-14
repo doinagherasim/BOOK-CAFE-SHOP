9b1b9c110805ac648c82f2db97992dc216e3c38d
Doina <gherasimdoina93@gmail.com>
2023-01-14 12:57:19 +0200

    BOOK CAFE- start

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ function App() {
     <div>
       <h2>WELCOME TO!</h2>
       <h3>BOOK CAFE!</h3>
+      <h5>Program:</h5>
+      <h5>Events:</h5>
+      <h5>Store:</h5>
+      <h5>Adress:</h5>
+      <h5>Contact:</h5>
     </div>
   );
 }
