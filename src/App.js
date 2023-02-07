452b8bf6997352f0dcab5d3eb99951ab513b7a48
Doina <gherasimdoina93@gmail.com>
2023-02-07 17:07:06 +0200

    book-cafe-in-progres-st

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
+import Header from "./components/Header"
+
 function App() {
   return (
     <div>
-      <h2>WELCOME TO!</h2>
-      <h3>BOOK CAFE!</h3>
-      <h5>Program:</h5>
-      <h5>Events:</h5>
-      <h5>Store:</h5>
-      <h5>Adress:</h5>
-      <h5>Contact:</h5>
+   <Header/>
     </div>
   );
 }
